3381411735ce01c4b0c2a640263f544e9c62bd26
Adesida Leye <adeleyeadesida@gmail.com>
2022-12-01 21:41:03 +0100

    made changes

--- script.js
+++
@@ -1,8 +0,0 @@
-let play = document.querySelector('.play');
-let pause = document.querySelector('.pause');
-
-play.addEventListener('click', () =>{
-    if(pause.classList.contains('seePause')){
-        pause.classList.toggle('seePause')
-    }
-})
